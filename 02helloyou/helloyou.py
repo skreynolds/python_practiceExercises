4eec38a3a9aea88410289f718ddae9f7e1cc1e87
Shane Reynolds <shane.k.reynolds@gmail.com>
2017-03-15 10:51:02 +1100

    Add a script which prompts the user for their name and greets them

---
+++ 02helloyou/helloyou.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 15 09:31:16 2017
+
+Write a program that asks the user for her name and greets her with her name.
+
+@author: shane
+"""
+name_in = input("Enter your name:")
+
+print("Hello " + name_in + "!")

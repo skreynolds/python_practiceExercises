665340c3c80b094c584058cf7a031f2dab99ae02
skreynolds <shane.k.reynolds@gmail.com>
2017-10-26 20:58:14 +0930

    Made changes to FK testing script

--- 15forwardKinematics/forwardKinematics.py
+++ 15forwardKinematics/forwardKinematics.py
@@ -8,6 +8,24 @@ Created on Mon Oct 23 22:35:58 2017
 
 from sympy import *
 
+def R_x(q):
+    M_x = Matrix([[ 1,      0,       0],
+                  [ 0, cos(q), -sin(q)],
+                  [ 0, sin(q),  cos(q)]])
+    return M_x
+
+def R_y(q):
+    M_y = Matrix([[ cos(q), 0, sin(q)],
+                  [      0, 1,       0],
+                  [-sin(q), 0, cos(q)]])
+    return M_y
+
+def R_z(q):
+    M_z = Matrix([[ cos(q), -sin(q), 0],
+                  [ sin(q),  cos(q), 0],
+                  [ 0,            0, 1]])
+    return M_z
+
 def transMat(q, alpha, d, a):
     T = Matrix([[           cos(q),           -sin(q),           0,             a],
                 [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
@@ -15,28 +33,31 @@ def transMat(q, alpha, d, a):
                 [                 0,                0,           0,             1]])
     return T
 
+### Create symbols for joint variables
 q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
 d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
 a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
 alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 
+### Populate fixed DH parameter table
+s = {alpha0:     0,    a0:      0,    d1:  0.75,
+     alpha1: -pi/2,    a1:   0.35,    d2:     0,    q2: (q2 - pi/2),
+     alpha2:     0,    a2:   1.25,    d3:     0,
+     alpha3: -pi/2,    a3: -0.054,    d4:   1.5,
+     alpha4:  pi/2,    a4:      0,    d5:     0,
+     alpha5: -pi/2,    a5:      0,    d6:     0,
+     alpha6:     0,    a6:      0,    d7: 0.303,    q7: 0}
 
-s = {alpha0:     0,    a0:      0,    d1: 0.68,
-     alpha1: -pi/2,    a1:   0.42,    d2:    0,    q2: (q2 - pi/2),
-     alpha2:     0,    a2:   1.25,    d3:    0,
-     alpha3: -pi/2,    a3: -0.054,    d4:  1.5,
-     alpha4:  pi/2,    a4:      0,    d5:    0,
-     alpha5: -pi/2,    a5:      0,    d6:    0,
-     alpha6:     0,    a6:      0,    d7: 0.11,    q7: 0}
-
+### Create homogeneous transfroms between neighbouring links
 T0_1 = transMat(q1, alpha0, d1, a0)
 T1_2 = transMat(q2, alpha1, d2, a1)
 T2_3 = transMat(q3, alpha2, d3, a2)
 T3_4 = transMat(q4, alpha3, d4, a3)
 T4_5 = transMat(q5, alpha4, d5, a4)
-T5_6 = transMat(q5, alpha5, d6, a5)
+T5_6 = transMat(q6, alpha5, d6, a5)
 T6_7 = transMat(q7, alpha6, d7, a6)
 
+### Substitute DH parameters into the homogeneous transforms
 T0_1 = T0_1.subs(s)
 T1_2 = T1_2.subs(s)
 T2_3 = T2_3.subs(s)
@@ -45,6 +66,8 @@ T4_5 = T4_5.subs(s)
 T5_6 = T5_6.subs(s)
 T6_7 = T6_7.subs(s)
 
+### Incrementally build the transform from the base link to the
+### gripper link (intrinsic rotation = post-mult)
 T0_2 = T0_1*T1_2
 T0_3 = T0_2*T2_3
 T0_4 = T0_3*T3_4
@@ -52,10 +75,44 @@ T0_5 = T0_4*T4_5
 T0_6 = T0_5*T5_6
 T0_7 = T0_6*T6_7
 
-R0_1 = T0_1[0:3,0:3]
-R1_2 = T1_2[0:3,0:3]
-R2_3 = T2_3[0:3,0:3]
-R3_4 = T3_4[0:3,0:3]
-R4_5 = T4_5[0:3,0:3]
-R5_6 = T5_6[0:3,0:3]
-R6_7 = T6_7[0:3,0:3]
+### Create the correction matrrix used to transform from urdf to DH
+R_Z = Matrix([[-1,  0, 0, 0],
+              [ 0, -1, 0, 0],
+              [ 0,  0, 1, 0],
+              [ 0,  0, 0, 1]])
+
+R_Y = Matrix([[0, 0, -1, 0],
+              [0, 1,  0, 0],
+              [1, 0,  0, 0],
+              [0, 0,  0, 1]])
+
+R_corr = R_Z*R_Y # intrinsic rotation = post
+
+### Numerically evaluate transforms
+### (compared with the output of tf_echo)
+print('\nT0_1 =\n')
+pprint(T0_1.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+print('\nT0_2 =\n')
+pprint(T0_2.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+print('\nT0_3 =\n')
+pprint(T0_3.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+print('\nT0_4 =\n')
+pprint(T0_4.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+print('\nT0_5 = \n')
+pprint(T0_5.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+print('\nT0_6 =\n')
+pprint(T0_6.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+print('\nT0_7 =\n')
+pprint(T0_7.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+
+### The corrected homogeneous tform from the base link to the end
+### effector
+T_total = T0_7*R_corr
+print('\nT0_7 =\n')
+pprint(T_total.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
+print(R_corr)
+
+#R_test = T3_4*T4_5*T5_6
+#R_test = R_x(q4)*R_y(q5)*R_x(q6)
+R_test = T0_3.evalf(subs={q1: 1, q2: .3, q3: .2})
+R0_3 = T0_3[0:3,0:3]

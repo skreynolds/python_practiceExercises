c1fe4e95e814317cc4c1508a7e8fdf7a5595c609
skreynolds <shane.k.reynolds@gmail.com>
2018-06-10 10:50:20 +0930

    Exercise 3 from ATBS

---
+++ 26ten_atbs/ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  6 19:44:15 2018
+
+@author: shane
+"""
+
+for i in range(10):
+    print(i+1)
+    
+i = 1
+
+while i != 11:
+    print(i)
+    i = i + 1
